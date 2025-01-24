c5234dd8731dce389b4d80852d71ec57f5978c29
Shoshan Dagany <shoshan@gmail.com>
2025-01-24 22:31:26 +0200

    lspconfig.lua: setup ruff client

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,10 +1,31 @@
+local lsp = vim.g.lazyvim_python_lsp or "pyright"
+local ruff = vim.g.lazyvim_python_ruff or "ruff"
+
+
 return {
-  "neovim/nvim-lspconfig",
-  opts = function(_, opts)
-    local servers = { "pyright",  "ruff" }
-    for _, server in ipairs(servers) do
-      opts.servers[server] = opts.servers[server] or {}
-      opts.servers[server].mason = false
-    end
-  end,
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      opts = opts or {}
+      local servers = { "pyright", "ruff" }
+      for _, server in ipairs(servers) do
+        opts.servers[server] = opts.servers[server] or {}
+        opts.servers[server].mason = false
+      end
+    end,
+  },
+ {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      opts = opts or {}
+      opts.setup = opts.setup or {}
+      -- Setup on_attach for the `ruff` LSP server
+      opts.setup[ruff] = function()
+        LazyVim.lsp.on_attach(function(client, _)
+          -- Disable hover in favor of Pyright
+          client.server_capabilities.hoverProvider = false
+        end) -- Close the function here without passing `ruff`
+      end
+    end,
+  },
 }
