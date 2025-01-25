e7e4104225e2b8f9a8b90490c24765849b32ee96
Shoshan Dagany <shoshan@gmail.com>
2025-01-25 18:51:12 +0200

    autocmds.lua: fix, autoformat opython files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,7 +7,7 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
+  pattern = "*.py",
   callback = function()
     local success, err = pcall(function()
       require("usr.git").format_modified_lines()
