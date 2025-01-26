224b39dac7b2979511d4b37dd34e5e004b3efd65
Shoshan Dagany <shoshan@gmail.com>
2025-01-26 21:18:05 +0200

    blink.cmp.lua: disable trigger on insert, leaving  completion for variables true

--- lua/plugins/blink.cmp.lua
+++ lua/plugins/blink.cmp.lua
@@ -2,6 +2,7 @@ return {
   "saghen/blink.cmp",
   opts = {
     completion = {
+      trigger = { show_on_keyword = true, prefetch_on_insert = false },
       ghost_text = {
         enabled = false,
       },
