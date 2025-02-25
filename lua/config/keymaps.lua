7d36761fe3c438f447966b37c61637d0cfb5f989
Shoshan Dagany <shoshan@gmail.com>
2025-02-25 14:27:11 +0200

    keymaps.lua: from config-nvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+--from dpetka
+local map = vim.keymap.set
+local del = vim.keymap.del
+
+--     ╭───────────────────────────────────────────────────────────────────╮
+--     │                        delete some lazy keymaps                   │
+--     ╰───────────────────────────────────────────────────────────────────╯
+del("n", "<leader>L")
+del("n", "<leader>l")
+-- del("n", "<leader>z")
+
