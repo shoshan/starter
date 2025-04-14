6a16adff4a22c1189027a96540a36bf0261936cd
Shoshan Dagany <shoshan@gmail.com>
2025-04-14 16:32:52 +0300

    colorschema normal comments green

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,9 +8,29 @@ return {
     "ellisonleao/gruvbox.nvim",
     opts = {
       overrides = {
-        ["@comment"] = { bg = "#1e1e1e" },
+        ["@comment"] = { bg = "#1e1e1e" }, -- Default background for comments
       },
     },
+    config = function()
+      -- Function to set comment highlight based on mode
+      local function set_comment_highlight()
+        local mode = vim.fn.mode()
+        if mode == "n" then
+          vim.api.nvim_set_hl(0, "@comment", { bg = "#004d00" }) -- Deep green for normal mode
+        else
+          vim.api.nvim_set_hl(0, "@comment", { bg = "#1e1e1e" }) -- Default for other modes
+        end
+      end
+
+      -- Initial highlight setup
+      set_comment_highlight()
+
+      -- Autocommand to update highlight on mode change
+      vim.api.nvim_create_autocmd({ "ModeChanged" }, {
+        pattern = "*",
+        callback = set_comment_highlight,
+      })
+    end,
   },
 
   -- Configure LazyVim to load gruvbox
